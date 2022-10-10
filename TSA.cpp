c80285e449842acd736872c44e4b6787019f65f9
Ryan Bjb <102423466+50t0r25@users.noreply.github.com>
2022-10-10 23:25:11 +0100

    comments

--- TSA.cpp
+++ TSA.cpp
@@ -24,17 +24,21 @@ string fileToString(const string &fileName) {
 //calculates Levenshtein Distance between 2 strings and returns the number of edits requires
 unsigned int edit_distance(const std::string& s1, const std::string& s2)
 {
+    //get sizes and puts them initializes matrix
 	const std::size_t len1 = s1.size(), len2 = s2.size();
 	std::vector<std::vector<unsigned int>> d(len1 + 1, std::vector<unsigned int>(len2 + 1));
 
+    //fill matrix
 	d[0][0] = 0;
 	for(unsigned int i = 1; i <= len1; ++i) d[i][0] = i;
 	for(unsigned int i = 1; i <= len2; ++i) d[0][i] = i;
 
+    //calculate cost
 	for(unsigned int i = 1; i <= len1; ++i)
 		for(unsigned int j = 1; j <= len2; ++j)
                       d[i][j] = std::min(std::min(d[i - 1][j] + 1, d[i][j - 1] + 1), d[i - 1][j - 1] + (s1[i - 1] == s2[j - 1] ? 0 : 1));
-	return d[len1][len2];
+	
+    return d[len1][len2]; //edit cost
 }
 
 
